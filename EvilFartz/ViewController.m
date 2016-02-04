8c589fd02bf690e024c0b3ed66878b93952c6b7e
automatousbeing <sjhennion@gmail.com>
2016-02-03 19:15:14 -0500

    Initial Commit

---
+++ EvilFartz/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EvilFartz
+//
+//  Created by Stephan Hennion on 2/3/16.
+//  Copyright © 2016 sjhennion. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ EvilFartz/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EvilFartz
+//
+//  Created by Stephan Hennion on 2/3/16.
+//  Copyright © 2016 sjhennion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
